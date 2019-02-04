b502aeb1e5a06987b5c072b7058f878f034d9eb2
FaresTrust <47318579+FaresTrust@users.noreply.github.com>
2019-02-04 12:32:13 +0200

    Fares Work
    Fares Work

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import {GlobalService}  from '../services/global.service';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  resultMessage:any;
+  constructor(public _GlobalService: GlobalService) {
+
+
+   }
+
+   SaveData(){
+  
+    let data={
+        "FullName": "Faresss1",
+        "EmailAddress": "fareselkh865@yahoo.com",
+        "PhoneNumber": "42364384",
+        "Password": "43243fd",
+        "CountryId": 81   
+    }
+
+   this.resultMessage=this._GlobalService.postService('s',data);
+
+console.log(data.FullName);
+
+   }
+
 
 }
