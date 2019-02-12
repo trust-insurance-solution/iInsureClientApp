f02541082ba8aa61f01a6d0cd6858eef0022ac21
FaresTrust <Fares@trst-ins.com>
2019-02-12 10:18:32 +0200

    Fares-Finish Code of Home business

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -12,31 +12,31 @@ import { NavController } from '@ionic/angular';
 export class HomePage implements OnInit {
   lang: string;
   userID: number;
-  accessToken: string = 'RXJOSXlwNjU4aDZzbVFlZ1FKUkMySEcydDBZRDJkTzhoam1EOGlwdXRjOD06Mi8xMS8yMDE5OjYzNjg1NDkzNjk1MTI0NTUyOQ==';
-  objHome = {  
-      Data:{
-    DateCreatedApartmentOrVilla: "2019-02-10",
-    DetailedLocation: "Fares",
-    Email: "fares@yahoo.com",
-    FkCreatedByUserId: 302,
-    FkTransactionId: 0,
-    Fk_SomeInsured: 4,
-    FullName: "Fares",
-    HowManyFloors: 0,
-    Images: [],
-    IsApartment: true,
-    IsOccuied: true,
-    IsRented: true,
-    NationalID: 81,
-    NumberOFRooms: 1,
-    NumberOfFloorsIfVilla: 5,
-    SizeOfApartmentOrVilla: 545,
-    Telephone: "fares",
-    WhichFloorIfApartment: 4,
+  accessToken: string = 'N1lVV0E2Yk5XM0dmS3ZuZGZLTUhvU0pWaXlDbzF3UE9Mbm40bTdmRm93ST06Mi8xMi8yMDE5OjYzNjg1NTU5NjI5ODI1MDEwMQ==';
+  objHome = {
+    Data: {
+      DateCreatedApartmentOrVilla: "",
+      DetailedLocation: "",
+      Email: "",
+      FkCreatedByUserId: 0,
+      FkTransactionId: 0,
+      Fk_SomeInsured: 0,
+      FullName: "",
+      HowManyFloors: 0,
+      Images: [],
+      IsApartment: true,
+      IsOccuied: true,
+      IsRented: true,
+      NationalID: 0,
+      NumberOFRooms: 0,
+      NumberOfFloorsIfVilla: 0,
+      SizeOfApartmentOrVilla: 0,
+      Telephone: "",
+      WhichFloorIfApartment: 0,
     },
-    Language: "en",
-    LoggedInUserID: 302,
-    };
+    Language: "",
+    LoggedInUserID: 0,
+  };
 
   constructor(public _GlobalService: GlobalService, public navCtrl: NavController) {
     this._GlobalService.getStorage('Lang').then((val) => {
@@ -47,19 +47,16 @@ export class HomePage implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   InsertHome() {
-   // this.objHome.Data.NationalID = this.objHome.Data.NationalID > 0 ? this.objHome.Data.NationalID : null;
-    //this.objHome.Language = this.lang;
-    //this.objHome.LoggedInUserID=this.userID;
-    //this.objHome.Data.FkCreatedByUserId = this.userID;
-    console.log(this.objHome);
+    this.objHome.Data.NationalID = this.objHome.Data.NationalID > 0 ? this.objHome.Data.NationalID : null;
+    this.objHome.Language = this.lang;
+    this.objHome.LoggedInUserID = this.userID;
+    this.objHome.Data.FkCreatedByUserId = this.userID;
     this.postInsertNewHome().then(res => {
-      console.log(res);
       if (res.Success === 'true') {
-        console.log(true);
-        this._GlobalService.showAlert('added successfully...', res.ErrorMessage, ['OK']);
+        this.navCtrl.navigateForward('GoToNutationsNotCreated');
       }
       else {
         this._GlobalService.showAlert('Failed...', res.ErrorMessage, ['OK']);
@@ -67,8 +64,7 @@ export class HomePage implements OnInit {
     }
     );
   }
-
-  public postInsertNewHome(): Promise<any> { 
+  public postInsertNewHome(): Promise<any> {
     return this._GlobalService.fetchDataApi('InsertNewHomeEntry', this.objHome, this.accessToken, this.userID.toString());
   }
 }
