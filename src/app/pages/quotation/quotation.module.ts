5168103d3415d7b3721a5a9dece4a9014f4541a4
tasneem995 <tasneem@trst-ins.com>
2019-02-18 15:26:47 +0200

    tasneem

---
+++ src/app/pages/quotation/quotation.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { QuotationPage } from './quotation.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: QuotationPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [QuotationPage]
+})
+export class QuotationPageModule {}
